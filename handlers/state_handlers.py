438823d625afcad400b3590a6995aaf5fbdeb757
Ilya Polotsky <ipolo.box@gmail.com>
2023-03-20 14:56:08 +0300

    Update default message response

--- handlers/state_handlers.py
+++ handlers/state_handlers.py
@@ -1,5 +1,5 @@
 #  Author: Ilya Polotsky (ipolo.box@gmail.com). Copyright (c) 2022.
-from telegram import Update
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext
 from mail import mail_service
 
@@ -8,7 +8,14 @@ from mail.mail_service import render_mail
 
 def show_state_text(update: Update, context: CallbackContext):
     update.message.reply_html(
-        f"Привет! Это бот IsraelWay. Переходи на сайт https://israelway.ru, чтобы зарегистрироваться",
-        disable_web_page_preview=True)
+        f"Привет!\n"
+        f"Это бот IsraelWay. Держи нашу <a href='https://bit.ly/iway-deck'>презентацию</a> - "
+        f"тут ответы на большинство вопросов, которые возможно у тебя возникнут.\n\n"
+        f"Если хочешь узнать что-то более подробно, то пиши вот сюда @israelway_IW",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Задать вопрос', url='https://t.me/israelway_IW')],
+            [InlineKeyboardButton(text='Наш сайт', url='https://israelway.ru')],
+        ]))
     return None
 
