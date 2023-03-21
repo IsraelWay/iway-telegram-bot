51a12e0c57e060b11fd3088af70fffd2277309a6
Ilya Polotsky <ipolo.box@gmail.com>
2023-03-21 16:51:43 +0300

    Fix grammar

--- handlers/state_handlers.py
+++ handlers/state_handlers.py
@@ -10,7 +10,7 @@ def show_state_text(update: Update, context: CallbackContext):
     update.message.reply_html(
         f"Привет!\n"
         f"Это бот IsraelWay. Держи нашу <a href='https://bit.ly/iway-deck'>презентацию</a> - "
-        f"тут ответы на большинство вопросов, которые возможно у тебя возникнут.\n\n"
+        f"тут ответы на большинство вопросов, которые, возможно, у тебя возникнут.\n\n"
         f"Если хочешь узнать что-то более подробно, то пиши вот сюда @israelway_IW",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([
